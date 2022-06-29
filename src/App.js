88da2391e5b824af8e7bd4b2ab7053a28b834c2b
Alex <alexbachurin91@gmail.com>
2022-06-29 18:38:46 +0300

    added sass + bg image to category

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import "./App.css";
 import "./categories.styles.scss";
+import categories from "./utils/categories.json";
 function App() {
-	const categories = [
-		{
-			id: 1,
-			title: "Hats",
-			img: "",
-		},
-		{
-			id: 2,
-			title: "Jackets",
-			img: "",
-		},
-		{
-			id: 3,
-			title: "Sneakers",
-			img: "",
-		},
-		{
-			id: 4,
-			title: "Womens",
-			img: "",
-		},
-		{
-			id: 5,
-			title: "Mens",
-			img: "",
-		},
-	];
-
 	return (
 		<div className="categories-container">
 			{categories.map((category) => {
-				const { id, title, img } = category;
+				const { id, title, imageUrl } = category;
 				return (
 					<div key={id} className="category-container">
-						<div className="background-image"></div>
+						<div
+							className="background-image"
+							style={{ backgroundImage: `url(${imageUrl})` }}
+						/>
 						<div className="category-body-container">
 							<h2>{title}</h2>
 							<p>Shop Now</p>
