1b875af8dfe7a6910415bbac96078c3f5230ee93
Alex <alexbachurin91@gmail.com>
2022-07-04 01:28:37 +0300

    added Links in CategoryPreview component

--- src/components/CategoryPreview/CategoryPreview.js
+++ src/components/CategoryPreview/CategoryPreview.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
 import "./categoryPreview.styles.scss";
 const CategoryPreview = ({ title, products }) => {
 	return (
 		<div className="category-preview-container">
 			<h2>
-				<span className="title">{title.toUpperCase()}</span>
+				<Link to={`${title}`}>
+					<span className="title">{title.toUpperCase()}</span>
+				</Link>
 			</h2>
 			{/* display only 4 products for each category */}
 			<div className="products">
