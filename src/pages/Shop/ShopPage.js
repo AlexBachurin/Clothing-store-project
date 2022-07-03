4255bfdac9901add9ab8ee231aaeb85b21f44b16
Alex <alexbachurin91@gmail.com>
2022-07-04 00:51:35 +0300

    added CategoryPreview component + styles

--- src/pages/Shop/ShopPage.js
+++ src/pages/Shop/ShopPage.js
@@ -2,32 +2,27 @@ import React from "react";
 import "./shopPage.styles.scss";
 import { useCategoriesContext } from "../../contexts/categoriesContext";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
 
 const ShopPage = () => {
 	const { categories } = useCategoriesContext();
-	console.log(Object.values(categories));
+
 	return (
-		<>
+		<div className="shop-container">
 			{
 				// use object keys to get the title from categories object
+				//and pass to CategoryPreview component title and associated products with this title
 				Object.keys(categories).map((title) => {
 					return (
-						<div key={title}>
-							<h2>{title}</h2>
-							<div className="products-container">
-								{/* iterate through values using key name of object, since value of this will be array we can map through it */}
-								{categories[title]
-									.map((category) => {
-										return <ProductCard key={category.id} {...category} />;
-										// also slice it to 4 positions
-									})
-									.slice(0, 4)}
-							</div>
-						</div>
+						<CategoryPreview
+							key={title}
+							title={title}
+							products={categories[title]}
+						/>
 					);
 				})
 			}
-		</>
+		</div>
 	);
 };
 
