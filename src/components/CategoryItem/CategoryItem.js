3f9cbf22f4c5726ff864298302ba1150e7784975
Alex <alexbachurin91@gmail.com>
2022-07-05 09:04:17 +0300

    added links to main page

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import Wrapper from "./Wrapper";
 
 const CategoryItem = ({ title, imageUrl }) => {
 	return (
 		<Wrapper imageUrl={imageUrl}>
 			<div className="background-image" />
-			<div className="category-body-container">
+			<Link to={`shop/${title}`} className="category-body-container">
 				<h2>{title}</h2>
 				<p>Shop Now</p>
-			</div>
+			</Link>
 		</Wrapper>
 	);
 };
