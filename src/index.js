dc1d56fde85b0520c56c7f86a45f91737ed9e333
Alex <alexbachurin91@gmail.com>
2022-07-07 05:35:54 +0300

    store setup

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/userContext";
 import { CategoriesProvider } from "./contexts/categoriesContext";
 import { CartProvider } from "./contexts/cartContext";
+import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
-				<CategoriesProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</CategoriesProvider>
-			</UserProvider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<UserProvider>
+					<CategoriesProvider>
+						<CartProvider>
+							<App />
+						</CartProvider>
+					</CategoriesProvider>
+				</UserProvider>
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,10 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
 
-//root-reducer
+//helper thar runs before the action hits reducer
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+//pass middleWare as 3rd argument, so we pass second argument as undefined
+export const store = createStore(rootReducer, undefined, composedEnhancers);
