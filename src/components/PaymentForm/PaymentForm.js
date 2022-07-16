5411de63d67fedeb636e2c0fa8390fb8c37c9269
Alex <alexbachurin91@gmail.com>
2022-07-16 04:18:03 +0300

    working stripe payment

--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -13,12 +13,40 @@ const PaymentForm = () => {
 		if (!stripe || !elements) {
 			return;
 		}
-
 		//fetch request to create payment intent
+		//with post method and body with amount to pay
+		const res = await fetch("/.netlify/functions/create-payment-intent", {
+			method: "post",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({ amount: 10000 }),
+		}).then((data) => data.json());
+		//get client secret
+		const clientSecret = res.paymentIntent.client_secret;
+		// payment result
+		const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+			payment_method: {
+				// card info
+				card: elements.getElement(CardElement),
+				//details
+				billing_details: {
+					name: "user name",
+				},
+			},
+		});
+		//check if we get an error back from stripe
+		if (paymentResult.error) {
+			alert(paymentResult.error);
+		} else {
+			if (paymentResult.paymentIntent.status === "succeeded") {
+				alert("Payment successfull");
+			}
+		}
 	};
 	return (
 		<Wrapper>
-			<form className="payment-form">
+			<form onSubmit={paymentHandler} className="payment-form">
 				<h2>Credit card payment</h2>
 				<CardElement />
 				<Button buttonType="inverted"> pay now</Button>
