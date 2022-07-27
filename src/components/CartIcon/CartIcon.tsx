3d4e3b4436d4d0cfd6db8ac37f91541f56aad12a
Alex <alexbachurin91@gmail.com>
2022-07-27 15:15:34 +0300

    added SVG typing

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
 import { ReactComponent as Icon } from "../../assets/cart-icon.svg";
 import { useSelector } from "react-redux";
 import Wrapper from "./Wrapper";
-const CartIcon = ({ handleOpenCart }) => {
-	const { cartCount } = useSelector((store) => store.cart);
+import { RootState } from "../../store/store";
+
+type CartIconProps = {
+	handleOpenCart: () => void;
+};
+
+const CartIcon: FC<CartIconProps> = ({ handleOpenCart }) => {
+	const { cartCount } = useSelector((store: RootState) => store.cart);
 	return (
 		<Wrapper onClick={handleOpenCart}>
 			<Icon className="cart-icon" />
