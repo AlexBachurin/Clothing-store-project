2ca41503de02b280f8cbe6f2d77f78a60cb8782f
Alex <alexbachurin91@gmail.com>
2022-07-28 18:17:04 +0300

    added code splitting/Suspence&Lazy/Dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./pages/Navigation/Navigation";
-import ShopPage from "./pages/Shop/ShopPage";
-import HomePage from "./pages/Home/HomePage";
-import AuthenticationPage from "./pages/Authentication/AuthenticationPage";
-import CheckoutPage from "./pages/Checkout/CheckoutPage";
-import ErrorPage from "./pages/Error/ErrorPage";
-import { useEffect } from "react";
-
+import { useEffect, lazy, Suspense } from "react";
 import ProtectedRoute from "./pages/ProtectedRoute/ProtectedRoute";
-
 import { useDispatch, useSelector } from "react-redux";
 import { checkUserSession } from "./store/user/userAction";
 import {
 	calculateTotalItems,
 	calculateTotalPrice,
 } from "./store/cart/cartAction";
+import Spinner from "./components/Spinner/Spinner";
+
+const HomePage = lazy(() => import("./pages/Home/HomePage"));
+const AuthenticationPage = lazy(() =>
+	import("./pages/Authentication/AuthenticationPage")
+);
+const CheckoutPage = lazy(() => import("./pages/Checkout/CheckoutPage"));
+const ShopPage = lazy(() => import("./pages/Shop/ShopPage"));
+const ErrorPage = lazy(() => import("./pages/Error/ErrorPage"));
+const Navigation = lazy(() => import("./pages/Navigation/Navigation"));
 
 function App() {
 	const dispatch = useDispatch();
@@ -41,22 +43,24 @@ function App() {
 		dispatch(calculateTotalPrice());
 	}, [cartItems, dispatch]);
 	return (
-		<Routes>
-			<Route path="/" element={<Navigation />}>
-				<Route index element={<HomePage />} />
-				<Route path="/shop/*" element={<ShopPage />} />
-				<Route
-					path="/auth"
-					element={
-						<ProtectedRoute>
-							<AuthenticationPage />
-						</ProtectedRoute>
-					}
-				></Route>
-				<Route path="/checkout" element={<CheckoutPage />} />
-			</Route>
-			<Route path="*" element={<ErrorPage />} />
-		</Routes>
+		<Suspense fallback={<Spinner />}>
+			<Routes>
+				<Route path="/" element={<Navigation />}>
+					<Route index element={<HomePage />} />
+					<Route path="/shop/*" element={<ShopPage />} />
+					<Route
+						path="/auth"
+						element={
+							<ProtectedRoute>
+								<AuthenticationPage />
+							</ProtectedRoute>
+						}
+					></Route>
+					<Route path="/checkout" element={<CheckoutPage />} />
+				</Route>
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</Suspense>
 	);
 }
 
